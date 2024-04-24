88b83e5923fcddbcb293cd98ed87b3299efb3821
Somto Ufondu <somtoufondu@gmail.com>
2024-04-24 07:09:02 -0600

    Add two new routes: a GET route to render the urls_new.ejs template and a POST route to handle the form submission.

--- express_server.js
+++ express_server.js
@@ -21,6 +21,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>");
 });
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -29,6 +35,11 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok'
 });
 
 app.listen(PORT, () => {
