24844c93ca57218840c1dc8a4b42f2ca11259e73
Somto Ufondu <somtoufondu@gmail.com>
2024-04-24 07:21:37 -0600

    Create a function to generate shortURL and update the POST /urls route to implement the function.

--- express_server.js
+++ express_server.js
@@ -4,6 +4,18 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  let result = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+ 
+  for (let i = 0; i < 6; i++) {
+    randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars.charAt(randomIndex);
+  }
+
+  return result;
+};
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -38,6 +50,11 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL; // Add new URL to database
+  res.redirect(`/urls/${shortURL}`); // Redirect to the new URL page
+  
   console.log(req.body); // Log the POST request body to the console
   res.send('Ok'); // Respond with 'Ok'
 });
